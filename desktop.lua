cbdd68129ceb88ff25a16e302b832be795dc23ce
Maeswoon <sendmasonmail@gmail.com>
2019-06-01 20:24:10 -0400

    reverted

--- desktop.lua
+++ desktop.lua
@@ -4,17 +4,16 @@
 
 -- Library Retrieval
 dofile("tgui.lua")
-local unicode = require("unicode")
-local computer = require("computer")
-local gpu = require("component").proxy(require("component").list("gpu")())
 tgui = _G.tgui
 local windows = tgui.windows
 local focusList = tgui.focusList
 local threads = tgui.threads
 local viewport = windows["viewport"]
-
+local gpu = require("component").proxy(require("component").list("gpu")())
+local computer = require("computer")
 local bg = gpu.getBackground()
 local fg = gpu.getForeground()
+local unicode = require("unicode")
 local hBar = function(x) return string.rep(unicode.char(0x2501), x) end
 local vBar = function(x) return string.rep(unicode.char(0x2503), x) end
 
@@ -22,17 +21,13 @@ local vBar = function(x) return string.rep(unicode.char(0x2503), x) end
 local menu = {}
 menu.drawn = false
 menu.items = {
-  { 
-    name = "beep",
+  { name = "beep",
     label = "Beep",
-    func = tgui.obj(function() computer.beep(300) end) 
-  },
-  { 
-    name = "add_window",
+    func = setmetatable({}, {__call = function() computer.beep(300) end}) },
+  { name = "add_window",
     label = "Add Window",
-    func = tgui.obj(function() tgui.addWindow("TestWindow", 16, 5, 128, 40, 0x000000, 0xFFFFFF, _, _) end) 
+    func = setmetatable({}, {__call = function() tgui.addWindow("TestWindow", 16, 5, 128, 40, 0x000000, 0xFFFFFF, _, _) end}) },
   }
-}
 
 --Setup Taskbar and Clock 
 viewport:fill("taskbar", 1, 48, 160, 3, 0xAFAFAF, 0x000000, " ")
@@ -46,54 +41,48 @@ for k, v in pairs(menu.items) do
 end
 viewport:set("menu_div-1", 1, 47 - (2 * #menu.items), 0xAFAFAF, 0x000000, hBar(18), false)
 viewport:hide("menu_div-1")
-viewport:addButton("menu", 1, 48, 8, 3, 0x009933, 0x000000, "Menu", tgui.obj(
-  function() 
-    viewport:toggle("menu_body")
-    viewport:toggle("menu_div-1")
-    for k, v in pairs(menu.items) do
-      viewport:toggle("button/menu_item-" .. v.name)
-      viewport:toggle("menu_div-"..k+1)
-    end
-    viewport.buffer()
+viewport:addButton("menu", 1, 48, 8, 3, 0x009933, 0x000000, "Menu", setmetatable({},{__call = function() 
+  viewport:toggle("menu_body")
+  viewport:toggle("menu_div-1")
+  for k, v in pairs(menu.items) do
+    viewport:toggle("button/menu_item-" .. v.name)
+    viewport:toggle("menu_div-"..k+1)
   end
-))
+  viewport.buffer()
+end}))
 viewport:set("clockbar_left", 149, 48, 0xAFAFAF, 0x000000, vBar(3), true)
 viewport:set("clockbar_right", 160, 48, 0xAFAFAF, 0x000000, vBar(3), true)
-threads["clock"] = tgui.obj(
-  function() 
-    viewport:set("time", 151, 49, 0xAFAFAF, 0x000000, os.date("%I"..":".."%M".." ".."%p"), false) 
-  end 
-)
+threads["clock"] = setmetatable({}, { __call = function() 
+  viewport:set("time", 151, 49, 0xAFAFAF, 0x000000, os.date("%I"..":".."%M".." ".."%p"), false) 
+end })
 
 -- Memory Counter
 viewport:set("memoryBar", 129, 48, 0xAFAFAF, 0x000000, vBar(3), true)
-threads.memoryCounter = tgui.obj(
-  function()
-    local fmStr = ""
-    local tmStr = ""
-    if math.min(math.log(computer.totalMemory() - computer.freeMemory()) / math.log(2)) < 20 then 
-      fmStr = math.max((computer.totalMemory() - computer.freeMemory()) / 2^10)
-      fmStr = tostring(fmStr):sub(1, 3)
-      fmStr = fmStr .. "KB" 
-    else
-      fmStr = math.max((computer.totalMemory() - computer.freeMemory()) / 2^20)
-      fmStr = tostring(fmStr):sub(1, 3)
-      fmStr = fmStr .. "MB" 
-    end
-    if math.min(math.log(computer.totalMemory()) / math.log(2)) < 20 then 
-      tmStr = math.max((computer.totalMemory()) / 2^10)
-      tmStr = tostring(tmStr):sub(1, 3)
-      tmStr = tmStr .. "KB" 
-    else 
-      tmStr = math.max((computer.totalMemory()) / 2^20)
-      tmStr = tostring(tmStr):sub(1, 3)
-      tmStr = tmStr .. "MB" 
-    end
-    fmStr = string.rep(" ", math.abs(5 - #fmStr)) .. fmStr
-    tmStr = tmStr .. string.rep(" ", math.abs(5 - #tmStr))
-    viewport:set("memoryCounter", 131, 49, 0xAFAFAF, 0x000000, "MEM: " .. fmStr .. " / " .. tmStr, false)
+threads.memoryCounter = setmetatable({}, {__call = function()
+  local fmStr = ""
+  local tmStr = ""
+  if math.min(math.log(computer.totalMemory() - computer.freeMemory()) / math.log(2)) < 20 then 
+    fmStr = math.max((computer.totalMemory() - computer.freeMemory()) / 2^10)
+    fmStr = tostring(fmStr):sub(1, 3)
+    fmStr = fmStr .. "KB" 
+  else
+    fmStr = math.max((computer.totalMemory() - computer.freeMemory()) / 2^20)
+    fmStr = tostring(fmStr):sub(1, 3)
+    fmStr = fmStr .. "MB" 
   end
-)
+  if math.min(math.log(computer.totalMemory()) / math.log(2)) < 20 then 
+    tmStr = math.max((computer.totalMemory()) / 2^10)
+    tmStr = tostring(tmStr):sub(1, 3)
+    tmStr = tmStr .. "KB" 
+  else 
+    tmStr = math.max((computer.totalMemory()) / 2^20)
+    tmStr = tostring(tmStr):sub(1, 3)
+    tmStr = tmStr .. "MB" 
+  end
+  fmStr = string.rep(" ", math.abs(5 - #fmStr)) .. fmStr
+  tmStr = tmStr .. string.rep(" ", math.abs(5 - #tmStr))
+  viewport:set("memoryCounter", 131, 49, 0xAFAFAF, 0x000000, "MEM: " .. fmStr .. " / " .. tmStr, false)
+end})
 
 logo = {
   "      ___          ___          ___     ",
@@ -113,16 +102,11 @@ for k, v in ipairs(logo) do
   viewport:set("logo_row"..tostring(k), 59, 19 + k, 0x54B8F7, 0x000000, v, false)
 end
 
-window_comps = {}
 for k, v in pairs(viewport.order) do
-  window_comps[k] = v
-end
-
-for k, v in pairs(window_comps) do
   viewport:set("viewport_debug_"..tostring(k), 10, 10, 0x54B8F7, 0x000000, v, false)
 end
 
-viewport:addButton("halt", 151, 1, 10, 3, 0xFF0000, 0x000000, "Halt", tgui.obj(function() tgui.halt() end)) 
+viewport:addButton("halt", 151, 1, 10, 3, 0xFF0000, 0x000000, "Halt", setmetatable({}, {__call = function() tgui.halt() end})) 
 
 -- Finalization
 tgui.refreshBuffer("viewport")
